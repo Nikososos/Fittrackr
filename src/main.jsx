e373ff2d01e630293b1c6cf945f3248c0a249a4b
Nikosos <nickywolf@live.nl>
2026-02-05 20:31:27 +0100

    wrapping AUTH provider in router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Authprovider } from './context/AuthContext';
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Authprovider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Authprovider>
   </React.StrictMode>
 );
