828b883651589021673aaf6a7656aed5b304fe85
Nikosos <nickywolf@live.nl>
2026-02-05 20:34:20 +0100

    importing usestates and css to loginpage

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import "./LoginPage.css";
+
 export default function LoginPage() {
     return <h1>Login</h1>;
 }
