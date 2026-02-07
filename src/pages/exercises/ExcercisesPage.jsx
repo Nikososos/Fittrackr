ad3160e8238c82f50effe23f02ee16e98e1a475f
Nikosos <nickywolf@live.nl>
2026-02-07 12:36:58 +0100

    adding dummy data in the code to work with untill the database is finished to continue building

--- src/pages/exercises/ExcercisesPage.jsx
+++ src/pages/exercises/ExcercisesPage.jsx
@@ -2,6 +2,42 @@ import { useMemo, useState } from "react";
 import AppLayout from "../../components/layout/AppLayout"
 import "./ExcercisesPage.css"
 
+const EXERCISES = [
+    {
+        id: "ex1",
+        name: "Bench Press (Barbell)",
+        equipment: "Barbell",
+        muscleGroups: ["Chest", "Shoulders", "Triceps"],
+        instructions: [
+            "Lay flat on the bench with your feet on the ground.",
+            "Lower the bar to your mid chest",
+            "press the bar up until your elbows are locked"
+        ],
+    },
+    {
+        id: "ex2",
+        name: "Dumbell Curl",
+        equipment: "Dumbells",
+        muscleGroups: ["Biceps"],
+        instructions: [
+            "Stand tall with dumbells at your side",
+            "Curl the weights up without swinging",
+            "Lower slowly and repeat",
+        ],
+    },
+    {
+        id: "ex3",
+        name: "Seated Cable Row",
+        equipment: "Cable",
+        muscleGroups: ["Back", "Biceps"],
+        instructions: [
+            "Sit with a neutral spine and grip the handle",
+            "Pull handle towards your torso",
+            "Control the return",
+        ],
+    },
+];
+
 export default function ExcercisesPage() {
 
     const selectedExercise =
