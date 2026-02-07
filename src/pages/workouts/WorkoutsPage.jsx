147b3bf842d9873612f95c8d066f34fc9504911c
Nikosos <nickywolf@live.nl>
2026-02-07 16:17:57 +0100

    setting up navigation to saved workouts

--- src/pages/workouts/WorkoutsPage.jsx
+++ src/pages/workouts/WorkoutsPage.jsx
@@ -1,8 +1,22 @@
-import AppLayout from "../../components/layout/AppLayout"
+import { useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AppLayout from "../../components/layout/AppLayout";
+import "./WorkoutsPage.css";
 
 export default function WorkoutsPage() {
+    const navigate = useNavigate();
+
+    // Demo data 
+    const [workouts, setWorkouts] = useState ([
+        { id: "w1", name: "Upperbody" },
+        { id: "w2", name: "Lowerbody" },
+    ]);
+
+    const [menuOpenForId, setMenuOpenforId] = useState(null);
+
+    
     return (
-        <AppLayout title="Home">
+        <AppLayout title="Workouts">
             <div>Workouts</div>
         </AppLayout>
     );
