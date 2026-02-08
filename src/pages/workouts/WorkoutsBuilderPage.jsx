acc55bd89df32a49377728cf875aab53d50c8dea
Nikosos <nickywolf@live.nl>
2026-02-08 12:04:25 +0100

    decided to use dummy data for WorkoutsBuilderPage untill db is finished

--- src/pages/workouts/WorkoutsBuilderPage.jsx
+++ src/pages/workouts/WorkoutsBuilderPage.jsx
@@ -1,3 +1,19 @@
+import { useMemo, useState } from "react";
+import { useParams } from "react-router-dom";
+import AppLayout from "../../components/layout/AppLayout";
+import "./WorkoutsBuilderPage.css";
+
+// Dummy data used, connect to db later//
+
+const EXCERCISE_LIBRARY = [
+    { id: "ex1", name: "Bench Press (Barbell)" },
+    { id: "ex2", name: "Dumbell Curl" },
+    { id: "ex3", name: "Seated Cable Row" },
+    { id: "ex4", name: "Lat Pulldown" },
+    { id: "ex5", name: "Shoulder Press" },
+];
+
+
 export default function WorkoutsBuilderPage() {
     return <h1>WorkoutBuilder</h1>;
 }
