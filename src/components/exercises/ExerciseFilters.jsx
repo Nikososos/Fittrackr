ac2d56b48d8f51a82b3cfaacee935a922e03f353
Nikosos <nickywolf@live.nl>
2026-02-09 17:41:23 +0100

    building filter function to excercisebrowsepanel

---
+++ src/components/exercises/ExerciseFilters.jsx
@@ -0,0 +1,35 @@
+import "./ExerciseFilters.css"
+
+export default function ExerciseFilters({
+    muscleGroupValue,
+    onMuscleGroupChange,
+    muscleGroupOptions = ["ALL"],
+    searchValue,
+    onSearchChange,
+    showMuscleGroup = true,
+}) {
+    return (
+        <div className="exFilters">
+            {showMuscleGroup && (
+                <select
+                    className="exSelect"
+                    value={muscleGroupValue}
+                    onChange={(e) => onMuscleGroupChange(e.target.value)}
+                >
+                    {muscleGroupOptions.map((m) => (
+                        <option key={m} value={m}>
+                            {m}
+                        </option>
+                    ))}
+                </select>
+            )}
+
+            <input
+                classNam="exSearch"
+                value={searchValue}
+                onChange={(e) = onSearchChange(e.target.value)}
+                placeholder="Search exercise"
+            />
+        </div>
+    )
+}
