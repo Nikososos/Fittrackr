c0e444342e2a6d6fbb1277f80565cdc80f65f6fe
Nikosos <nickywolf@live.nl>
2026-02-11 20:20:11 +0100

    setting up imports for SettingsPage

--- src/pages/settings/SettingsPage.jsx
+++ src/pages/settings/SettingsPage.jsx
@@ -1,6 +1,11 @@
-import AppLayout from "../../components/layout/AppLayout"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AppLayout from "../../components/layout/AppLayout";
+import { useAuth } from "../../context/AuthContext";
+import "./SettingsPage.css";
 
 export default function SettingsPage() {
+    const
     return (
         <AppLayout title="Home">
             <div>Settings</div>
