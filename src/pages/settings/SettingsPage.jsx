7f4a4b1b0b80c02a7b76f7a6588499efa44372f4
Nikosos <nickywolf@live.nl>
2026-02-11 20:24:00 +0100

    added logut function, change email function and change password function

--- src/pages/settings/SettingsPage.jsx
+++ src/pages/settings/SettingsPage.jsx
@@ -5,7 +5,25 @@ import { useAuth } from "../../context/AuthContext";
 import "./SettingsPage.css";
 
 export default function SettingsPage() {
-    const
+    const navigate = useNavigate();
+    const { logout } = useAuth();
+
+    // Demo data, (later connect with Auth token from backend)
+    const [email] = useState("user@email.com");
+
+    function handleLogout() {
+        logout();
+        navigate("/login");
+    }
+
+    function handleChangeEmail() {
+        alert("Open change email modal/form later");
+    }
+
+    function handleChangePassword() {
+        alert("Open change password modal/form")
+    }
+    
     return (
         <AppLayout title="Home">
             <div>Settings</div>
