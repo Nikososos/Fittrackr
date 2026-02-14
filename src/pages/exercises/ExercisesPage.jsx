560dd41205c48c0fda7f064238b76f4f9385f0fb
Nikosos <nickywolf@live.nl>
2026-02-14 17:39:24 +0100

    removing the add to workout button on excercisepage completely, adding excercises to workout fully goes via the workoutbuilderPage

--- src/pages/exercises/ExercisesPage.jsx
+++ src/pages/exercises/ExercisesPage.jsx
@@ -120,8 +120,6 @@ export default function ExercisesPage() {
                                     ))}
                                 </ol>
                             </div>
-
-                            <button className="primaryBtn">Add to workout</button>
                         </>
                        )
                     )}
