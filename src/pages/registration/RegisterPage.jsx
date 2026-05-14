3754e26f2432730cac7f8e03fc07fe3dbfb74f14
Nikosos <nickywolf@live.nl>
2026-05-14 14:08:02 +0200

    adding final html content for registration page

--- src/pages/registration/RegisterPage.jsx
+++ src/pages/registration/RegisterPage.jsx
@@ -1,55 +1,74 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { noviFetch } from "../../api/noviClient";
 import "/RegisterPage.css";
 
 export default function RegisterPage() {
     return (
         <div className="loginPage">
-            <h1 className="loginTitle">FitTrackr</h1>
+            <div className="loginCard" role="dialog" aria-label="Register">
+                <h1 className="loginTitle">FitTrackr</h1>
 
-            <form className="loginForm" onSubmit={handleSubmit}>
-                <label className="fieldLabel" htmlFor="email">
-                    Email Adress
-                </label>
-                <input
-                    id="email"
-                    className="textInput"
-                    type="email"
-                    placeholder="name@email.com"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    autocomplete="email"
-                />
+                <form className="loginForm" onSubmit={handleSubmit}>
+                    <label className="fieldLabel" htmlFor="email">
+                        Email Adress
+                    </label>
+                    <input
+                        id="email"
+                        className="textInput"
+                        type="email"
+                        placeholder="name@email.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        autocomplete="email"
+                    />
 
-                <label className="fieldLabel" htmlFor="password">
-                    Password
-                </label>
-                <input
-                    id="password"
-                    className="textInput"
-                    type="password"
-                    placeholder="*******"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    autocomplete="new-password"
-                />
+                    <label className="fieldLabel" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        id="password"
+                        className="textInput"
+                        type="password"
+                        placeholder="*******"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        autocomplete="new-password"
+                    />
 
-                <label className="fieldLabel" htmlFor="confirmPassword">
-                    Verify Password
-                </label>
-                <input
-                    id="confirmPassword"
-                    className="textInput"
-                    type="password"
-                    placeholder="*******"
-                    value={confirmPassword}
-                    onChange={(e) => setEmail(e.target.value)}
-                    autocomplete="new-password"
-                />
+                    <label className="fieldLabel" htmlFor="confirmPassword">
+                        Verify Password
+                    </label>
+                    <input
+                        id="confirmPassword"
+                        className="textInput"
+                        type="password"
+                        placeholder="*******"
+                        value={confirmPassword}
+                        onChange={(e) => setEmail(e.target.value)}
+                        autocomplete="new-password"
+                    />
 
-                
-            </form>
+                    {error && <div className="errobanner">{error}</div>}
+
+                    <div className="buttonRow">
+                        <button
+                            className="btnSecondary"
+                            type="button"
+                            onClick={() => Navigate("/login")}
+                        >
+                            Back to login
+                        </button>
+                        <button
+                            className="btnPrimary"
+                            type="button"
+                            disabled={isLoading}
+                        >
+                            {isLoading ? "Registering..." : "Register account"}
+                        </button>
+                    </div>
+                </form>
+            </div>    
         </div>
-    )
+    );
 }
