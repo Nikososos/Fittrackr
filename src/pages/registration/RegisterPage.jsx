6be81ed9147d57be429c05b2c38a410a3d613dd7
Nikosos <nickywolf@live.nl>
2026-05-14 13:41:51 +0200

    setting up registration page

---
+++ src/pages/registration/RegisterPage.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { noviFetch } from "../../api/noviClient";
+import "/RegisterPage.css";
+
+export default function RegisterPage() {
+    
+}
